4ed5a1c631c1b5aeee7bb3136cbac11c60bc74ce
KaimuSeino <skim07a19yki@icloud.com>
2026-06-03 21:33:37 +0900

    vramをルート直下に移動しPort A, Port Bを追加

--- vram.v
+++ vram.v
@@ -2,11 +2,16 @@
 
 module vram (
   input  wire        clk,
+
+  // Port A: CPU 用(書き込み + 読み出し)
   input  wire [12:0] addr_a,
   input  wire [15:0] data_in,
   input  wire        we,
+  output reg  [15:0] data_out_a,
+
+  // Port B: display_fsm 用(読み出し専用)
   input  wire [12:0] addr_b,
-  output reg  [15:0] data_out
+  output reg  [15:0] data_out_b
 );
 
   reg [15:0] mem [8191:0];
@@ -17,7 +22,8 @@ module vram (
 
   always @(posedge clk) begin
     if (we) mem[addr_a] <= data_in;
-    data_out <= mem[addr_b];
+    data_out_a <= mem[addr_a];
+    data_out_b <= mem[addr_b];
   end
 
 endmodule
